a71f7e10bc5c24b457b2baca536395ad9d97b162
darkirby <darkirby.tuta.com>
2025-12-15 23:05:48 +0100

    Standardize config for tests

--- docker-db-init.sql
+++
@@ -1,7 +0,0 @@
-CREATE DATABASE IF NOT EXISTS hub_db;
-CREATE DATABASE IF NOT EXISTS hub_db_test;
-
-CREATE USER  IF NOT EXISTS 'hub_user'@'%' IDENTIFIED BY 'hub_password';
-
-GRANT ALL PRIVILEGES ON hub_db.* TO 'hub_user'@'%';
-GRANT ALL PRIVILEGES ON hub_db_test.* TO 'hub_user'@'%';
